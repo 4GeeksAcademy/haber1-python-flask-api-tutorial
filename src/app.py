72d303cae683348164f00c975972321d08935d52
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-07-05 09:04:51 +0000

    Entregado

--- src/app.py
+++ src/app.py
@@ -1,12 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/todos',methods=['GET'])
-def hello_world():
-    return jsonify(todos)
+@app.route('/todos', methods=['GET', 'POST'])
+def todos():
+    response_body = {}
+    if request.method == 'GET':
+        response_body['message'] = 'Lista de todos'
+        response_body['results'] = todos
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json
+        todos.append(data)
+        response_body['message'] ='Todo agregado correctamente'
+        response_body['results'] = todos
+        return response_body, 201
+
+
+@app.route('/todos/<int:position>', methods=['GET', 'PUT', 'DELETE'])
+def todo(position):
+    response_body = {}
+    if position > len(todos):
+        response_body['message'] = 'Posición fuera de rango'
+        return response_body, 400
+    if request.method == 'DELETE':
+        del todos[position]
+        response_body['message'] = f'This is the position to delete {position}'
+        return response_body, 200
+    response_body['message'] = 'El GET Y el PUT no esrtan defenidos aún'
+    return response_body, 200
+ 
 
 todos = [{'label': 'My firt task', 'done': False},
         {'label': 'My second task', 'done': False}]
